b96d558757d254ab2ac0292874f4526a74c15fe0
Yuhri Graziano Bernardes <yuhri.graziano@gmail.com>
2022-07-25 22:47:17 -0300

    refact: rearange folder structure

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,7 +1,7 @@
-import 'package:bytebank/pages/contact_list.dart';
+import 'package:bytebank/pages/contacts/contact_list.dart';
 import 'package:bytebank/pages/dashboard.dart';
-import 'package:bytebank/pages/transfer_form.dart';
-import 'package:bytebank/pages/transfers_list.dart';
+import 'package:bytebank/pages/transfers/transfer_form.dart';
+import 'package:bytebank/pages/transfers/transfers_list.dart';
 import 'package:flutter/material.dart';
 
 final routes = <String, WidgetBuilder>{
