598565d1f8bf40a57256535d4ae5f946a2fa6c68
Yuhri Graziano Bernardes <yuhri.graziano@gmail.com>
2022-07-25 22:23:36 -0300

    feat: Contact List

---
+++ lib/models/contact.dart
@@ -0,0 +1,7 @@
+class Contact {
+  final String name;
+  final String account_number;
+  final String account_branch;
+
+  Contact(this.name, this.account_number, this.account_branch);
+}
---
+++ lib/pages/contact_list.dart
@@ -0,0 +1,56 @@
+import 'package:bytebank/models/contact.dart';
+import 'package:flutter/material.dart';
+
+class ContactsList extends StatefulWidget {
+  final List<Contact> _contacts = <Contact>[
+    Contact("Pedrinho", "0001", "01010101-1"),
+    Contact("Pedrinho", "0001", "01010101-1"),
+    Contact("Pedrinho", "0001", "01010101-1"),
+    Contact("Pedrinho", "0001", "01010101-1"),
+  ];
+
+    ContactsList({Key? key}) : super(key: key);
+
+  @override
+  State<ContactsList> createState() => _ContactsListState();
+}
+
+class _ContactsListState extends State<ContactsList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+      appBar: AppBar(title: const Text("Contacts")),
+      body: ListView.builder(
+        itemCount: widget._contacts.length,
+        itemBuilder: (BuildContext ctx, int idx) {
+          return ContactListItem(widget._contacts[idx]);
+        },
+      ),
+    );
+  }
+}
+
+class ContactListItem extends StatelessWidget {
+  final Contact contact;
+
+  const ContactListItem(this.contact, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(
+          contact.name,
+          style: const TextStyle(fontSize: 24.0, fontWeight: FontWeight.w800),
+        ),
+        subtitle: Text(
+            "Branch: ${contact.account_branch} Number: ${contact.account_number}",
+            style: const TextStyle(color: Colors.grey)),
+      ),
+    );
+  }
+}
